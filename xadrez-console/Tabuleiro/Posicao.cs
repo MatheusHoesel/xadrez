202030e8973be47710b00a88f2d93e4b51025456
Matheus Hoesel Dias <matheushoeseldias@gmail.com>
2020-08-16 21:17:49 -0300

    Classe Criada

---
+++ xadrez-console/Tabuleiro/Posicao.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Principal;
+using System.Text;
+
+namespace xadrez_console.Tabuleiro
+{
+    class Posicao
+    {
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+
+        public Posicao()
+        {
+        }
+
+        public Posicao(int linha, int coluna)
+        {
+            Linha = linha;
+            Coluna = coluna;
+        }
+
+        public override string ToString()
+        {
+            return "Linha: " + Linha
+                + ", Coluna: " + Coluna;
+        }
+    }
+}
